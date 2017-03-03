a391456c29f0fba2ef0403d423eb812a4e8bfd41
Joshua Seitz <SeitzJoshua7@gmail.com>
2017-03-03 14:20:46 +0800

    first commit

--- browser.rb
+++ browser.rb
@@ -1,16 +1,22 @@
-require 'net/http'
-require 'nokogiri'
 
-require_relative 'util'
+require_relative 'page'
 
 class Browser
   def run!
     # Run the browser
     # Display a prompt for a user
+    puts "What is your url link?"
     # Parse their input
+    input = gets.chomp
+    page = Page.new(input)
+
     # Display useful results by instantiating
     #   a new Page and calling methods on it.
     
+    page.url
+    page.fetch!
+    page.title
+    page.links
     # Questions:
     #  1. How can a user quit the browser gracefully?
     #  2. How can a user ask for help, i.e., how do they know what commands are available to them?
--- page.rb
+++ page.rb
@@ -1,17 +1,38 @@
+require 'uri'
+require 'net/http'
+require 'nokogiri'
+require 'open-uri'
+
 class Page
   def initialize(url)
+    @url = url
+    @page
   end
   
+  def url
+    puts "url> #{@url}"
+  end
+
   def fetch!
+    uri = URI.parse(@url)
+    puts "Fetching..."
+    @page = Nokogiri::HTML(open(uri))
   end
   
   def title
+    puts "Title: #{@page.css('title')}"
+    temp = @page.text
+    puts "Content length: #{temp.length}"
   end
   
   def links
     # Research alert!
     # How do you use Nokogiri to extract all the link URLs on a page?
-    #
+    links = @page.css('a')
+    puts "Links:"
+    links.each do |link|
+      puts " #{link['href']}"
+    end
     # These should only be URLs that look like
     #   <a href="http://somesite.com/page.html">Click here!</a>
     # This would pull out "http://somesite.com/page.html"
